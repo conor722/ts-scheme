d65790b9ec5f2fa7a08708d8f2f97755215a2d46
conor <conormckechnie234@gmail.com>
2025-11-23 22:53:07 +0000

    error handling in REPL instead of just crashing out

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import * as readline from "node:readline";
-import {interpret} from "./interpreter";
-import {parse} from "./parse";
+import { interpret } from "./interpreter";
+import { parse } from "./parse";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const repl = () => {
-    console.log("Really simple scheme interpretation! - type 'exit' to leave");
-    rl.question(`>`, input => {
-        if (input === "exit") {
-            return rl.close();
-        }
+  console.log("Really simple scheme interpretation! - type 'exit' to leave");
+  rl.question(`>`, (input) => {
+    if (input === "exit") {
+      return rl.close();
+    }
 
-        console.log(interpret(parse(input)));
-        repl();
-    });
+    try {
+      console.log(interpret(parse(input)));
+    } catch (error: any) {
+      console.error(error.message);
+    }
+    repl();
+  });
 };
 
 repl();
